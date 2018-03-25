29be87330000f9fd170e328f90d938e188d4ebb2
euphoriakate <ekaterina.kuzmina@prosphero.io>
2018-03-25 19:52:21 +0300

    get_coin_list get_coin_price

--- run.py
+++ run.py
@@ -1,15 +1,20 @@
 import pprint
 from cryptoCompare import cryptoCompare
+import psycopg2
+
 
 
 #pprint.pprint(cryptoCompare_old.get_price(from_currency='ETH', to_currency='KICK'))
 
 if __name__ == '__main__':
-    cryptoCompare = cryptoCompare()
+    #cryptoCompare = cryptoCompare()
     #pprint.pprint(cryptoCompare.get_coin_info())
     #pprint.pprint(cryptoCompare.get_coin_price('BTC', 'USD', 'CoinBase'))
     #pprint.pprint(cryptoCompare.get_coin_multiprice('BTC,ETH,LTC', 'USD,KICK'))
     #pprint.pprint(cryptoCompare.get_coin_pricemultifull())
-    pprint.pprint(cryptoCompare.get_avg('ETH', 'BTC', exchange='Kraken'))
+    #pprint.pprint(cryptoCompare.get_avg('ETH', 'BTC', exchange='Kraken'))
 
+    #host = "localhost", database = "suppliers", user = "postgres", password = "postgres"
+    conn = psycopg2.connect(filename = 'database.ini', section = )
+    pprint.pprint(conn)
 
